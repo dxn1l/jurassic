a93936dceb11e64b189888db4a9c0c17d1437e31
dxn1l <danielmorenorey2012@gmail.com>
2024-11-13 11:28:01 +0100

    Initial commit

---
+++ src/test/java/com/example/jurassic/JurassicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jurassic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
