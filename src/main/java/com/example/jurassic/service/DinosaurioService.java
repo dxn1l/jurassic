4ca778c8f44667ab79ef7e8ea36626aa84d60f1f
dxn1l <danielmorenorey2012@gmail.com>
2024-11-13 22:01:32 +0100

    added log on distribution

--- src/main/java/com/example/jurassic/service/DinosaurioService.java
+++ src/main/java/com/example/jurassic/service/DinosaurioService.java
@@ -3,6 +3,8 @@ package com.example.jurassic.service;
 import com.example.jurassic.entity.Dinosaurio;
 import com.example.jurassic.repository.DinosaurioRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,8 @@ public class DinosaurioService {
 
     @Autowired
     private IslaAcuaticaService islaAcuaticaService;
+
+    private static final Logger logger = LoggerFactory.getLogger(IslaCriaService.class);
 
     public DinosaurioService(DinosaurioRepository dinosaurioRepository) {
         this.dinosaurioRepository = dinosaurioRepository;
@@ -49,12 +53,15 @@ public class DinosaurioService {
         switch (dinosaurio.getTipoHabitat()) {
             case "VOLADOR":
                 islaVoladoraService.agregarDinosaurio(dinosaurio);
+                logger.info("Dinosaurio {} con tipo {} distribuido en isla voladora", dinosaurio.getId() , dinosaurio.getTipoHabitat());
                 break;
             case "TERRESTRE":
                 islaTerrestreService.agregarDinosaurio(dinosaurio);
+                logger.info("Dinosaurio {} con tipo {} distribuido en isla terrestre", dinosaurio.getId() , dinosaurio.getTipoHabitat());
                 break;
             case "ACUATICO":
                 islaAcuaticaService.agregarDinosaurio(dinosaurio);
+                logger.info("Dinosaurio {} con tipo {} distribuido en isla acuática", dinosaurio.getId() , dinosaurio.getTipoHabitat());
                 break;
             default:
                 throw new IllegalArgumentException("Tipo de habitat desconocido: " + dinosaurio.getTipoHabitat());
