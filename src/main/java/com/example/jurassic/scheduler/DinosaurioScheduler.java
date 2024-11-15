c67c108b1f2427dd16ce317cd326ed1f60409c7a
Rodrivdlc <rodri.vdlc@gmail.com>
2024-11-15 21:40:09 +0100

    edad de muerte randomizada

--- src/main/java/com/example/jurassic/scheduler/DinosaurioScheduler.java
+++ src/main/java/com/example/jurassic/scheduler/DinosaurioScheduler.java
@@ -9,6 +9,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.util.List;
 import com.example.jurassic.service.DinosaurioService;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 @Component
 public class DinosaurioScheduler {
@@ -25,6 +27,58 @@ public class DinosaurioScheduler {
     @Autowired
     private IslaVoladoraService islaVoladoraService;
 
+    @Scheduled(fixedRate = *) // Cada 10 segundos
+    public void actualizarEdadDinosauriosEIslas() {
+        List<Dinosaurio> dinosaurios = dinosaurioService.obtenerTodosDinosaurios();
+
+        for (Dinosaurio dino : dinosaurios) {
+            dino.setEdad(dino.getEdad() + 1);
+
+            // Generar una edad máxima dinámica dentro del rango (20-30)
+            int edadMaxima = ThreadLocalRandom.current().nextInt(20, 40);
+
+            // Actualizar la edad de la isla correspondiente
+            switch (dino.getTipoHabitat()) {
+                case "TERRESTRE":
+                    islaTerrestreService.incrementarEdadIsla(dino.getId());
+                    break;
+                case "ACUATICO":
+                    islaAcuaticaService.incrementarEdadIsla(dino.getId());
+                    break;
+                case "VOLADOR":
+                    islaVoladoraService.incrementarEdadIsla(dino.getId());
+                    break;
+            }
+
+            // Si el dinosaurio alcanza o supera la edad máxima, eliminarlo
+            if (dino.getEdad() >= edadMaxima) {
+                dinosaurioService.eliminarDinosaurio(dino);
+
+                switch (dino.getTipoHabitat()) {
+                    case "TERRESTRE":
+                        islaTerrestreService.eliminarDinosaurio(dino);
+                        break;
+                    case "ACUATICO":
+                        islaAcuaticaService.eliminarDinosaurio(dino);
+                        break;
+                    case "VOLADOR":
+                        islaVoladoraService.eliminarDinosaurio(dino);
+                        break;
+                }
+
+                dinosaurioService.enviarMensajeMuerte(dino);
+            } else {
+                dinosaurioService.guardarDinosaurio(dino);
+            }
+        }
+    }
+
+
+
+
+
+
+    /*
     @Scheduled(fixedRate = 10000) // Cada 10 segundos
     public void actualizarEdadDinosauriosEIslas() {
         // Incrementar edad de dinosaurios
@@ -66,5 +120,5 @@ public class DinosaurioScheduler {
                 dinosaurioService.guardarDinosaurio(dino);
             }
         }
-    }
+    }*/
 }
